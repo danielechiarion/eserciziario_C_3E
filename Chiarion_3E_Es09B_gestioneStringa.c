6c47b82297ec2a41dc27fb7277590c0e89394f46
il deniel <150442406+danielechiarion@users.noreply.github.com>
2024-03-27 12:28:31 +0100

    new exercise to operate with strings

---
+++ Chiarion_3E_Es09B_gestioneStringa.c
@@ -0,0 +1,137 @@
+// Author: Daniele Chiarion
+// Date: 27-03-2024
+
+/* Scrivere un programma in C che dopo aver inserito una stringa a piacere permetta di:
+   1) Eliminare 1 carattere scelto dall’utente ( o   più di 1 se si ripetono);
+   2) Eliminare 1 carattere scelto dall’utente in  base alla sua posizione;
+   3) Aggiungere 1  carattere scelto dall’utente alla fine della stringa; */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* funzione per menu generico */
+int menu(char stringa[][50], int size)
+{
+    int scelta;
+    int i;
+
+    do
+    {
+        printf("\n=== %s ===", stringa[0]); // output of the title
+        for (i = 1; i < size; i++)
+        {
+            printf("\n%s", stringa[i]); // output of the options
+        }
+        printf("\n");
+        scanf("%d", &scelta);
+        /* error message */
+        if (scelta < 1 || scelta > size - 1)
+        {
+            printf("\nValore errato. Riprova");
+            sleep(3);
+            ClrScr();
+        }
+    } while (scelta < 1 || scelta > size - 1); // checking value
+
+    return scelta;
+}
+
+/* funzione per pulire lo schermo */
+void ClrScr()
+{
+#ifdef _WIN32      // Check if the OS is Windows
+    system("cls"); // instruction for clearing cmd of Windows
+#else
+    system("clear"); // instruction for clearing shell of Linux / MacOS
+#endif
+}
+
+/* funzione che cancella carattere
+in base alla posizione */
+void cancellaPos(char stringa[], int pos)
+{
+    /* parto dalla posizione prevista e
+    scorro tutti i caratteri*/
+    for (int i = pos + 1; i < strlen(stringa); i++)
+        stringa[i - 1] = stringa[i]; // scorro i caratteri nella posizione precedente
+}
+
+/* funzione per cancellare
+i caratteri uguali nella stringa */
+void cancellaCarattere(char stringa[], char carattere)
+{
+    /* scorro tutti i caratteri per cercare
+    quello richiesto */
+    for (int i = 0; i < strlen(stringa); i++)
+    {
+        if (stringa[i] == carattere) // se lo strovo, sposto tutti i caratteri verso sinistra
+            cancellaPos(stringa, i);
+    }
+}
+
+void main()
+{
+    /* dichiarazione variabili */
+    char stringa[100];
+    // menu di scelta utente
+    char inputChar;
+    int posizione;
+    int scelta;
+    char opzioni[][200] = {{"OPZIONI PER L'UTENTE"},{"[1] Eliminare un carattere scelto dall'utente"},{"[2] Eliminare un carattere scelto dall'utente in base alla posizione"},{"[3] Aggiungere un carattere scelto a fine stringa"},{"[4] Fine"}};
+
+    /* input dati */
+    printf("Inserisci la stringa: ");
+    scanf("%s", stringa);
+
+    scelta = menu(opzioni, sizeof(opzioni) / sizeof(opzioni[0]));
+    ClrScr();
+
+    /* ripetizione operazioni
+    fino a quando non si sceglie di terminare */
+    do
+    {
+        /* switch case con le varie opzioni */
+        switch (scelta)
+        {
+        case 1:
+            /* input carattere */
+            printf("Inserisci carattere da eliminare: ");
+            scanf("%c", &inputChar);
+            /* modifica stringa */
+            cancellaCarattere(stringa, inputChar);
+            /* output risultati */
+            printf("\n\nLa stringa modificata e' %s", stringa);
+            break;
+        case 2:
+            do
+            { /* input valori */
+                printf("Inserisci posizione del carattere da eliminare ");
+                scanf("%d", &posizione);
+
+                /* possibile messaggio di errore */
+                if (posizione < 0 || posizione >= strlen(stringa))
+                    printf("\nIl valore non appartiene all'intervallo richiesto.\n\n");
+            } while (posizione < 0 || posizione >= strlen(stringa)); // controllo se il valore è corretto
+            /* modifica stringa */
+            cancellaPos(stringa, posizione);
+            /* output risultati */
+            printf("\n\nLa stringa modificata e' %s", stringa);
+            break;
+        case 3:
+            /* input carattere */
+            printf("Inserisci carattere da aggiungere a fine stringa: ");
+            scanf("%c", &inputChar);
+            /* modifica stringa
+            e concatenazione carattere*/
+            strcat(stringa, inputChar);
+            /* output risultati */
+            printf("\n\nLa stringa modificata e' %s", stringa);
+            break;
+        default:
+            printf("arresto del sistema");
+            sleep(3);
+        }
+    } while (scelta!=strlen(opzioni)-1);
+}
